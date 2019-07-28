f2e37bdb5852127d54fb66c138ae799d3bcfbdce
Arllain <arllain@gmail.com>
2019-07-28 01:24:40 -0300

    creating a ToggleLink component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,6 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 import { ShopConnector } from './shop/ShopConnector';
 import './App.css';
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          Sport Store
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
-
 export default class App extends Component {
 
   render() {
--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect} from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { loadData } from "../data/ActionCreators";
 import { DataTypes } from "../data/Types";
@@ -14,20 +14,20 @@ const mapDispatchToProps = {
 }
 
 const filterProducts = (products = [], category) => 
-    (!category || category === 'All') 
+    (!category || category === "All") 
     ? products
-    : products.filter( p => p.category.toLowerCase() === category.toLowerCase());
+    : products.filter(p => p.category.toLowerCase() === category.toLowerCase());
 
 export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
     class extends Component {
         render() {
             return <Switch>
-                <Route path='/shop/product/:category?'
+                <Route path="/shop/products/:category?"
                     render={ (routeProps) =>
-                        <Shop { ...this.props } {...routeProps}
+                        <Shop { ...this.props } {...routeProps }
                             products={ filterProducts(this.props.products,
                             routeProps.match.params.category) } /> } />
-                <Redirect to='/shop/products'/>
+                <Redirect to="/shop/products"/>
             </Switch>
         }
 
