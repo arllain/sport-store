09f14b663126583abcff8ba52286b2f81d2d0353
arllain <arllain@gmail.com>
2019-07-31 08:27:20 -0300

    Crating the pagination controls

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -1,9 +1,11 @@
 import Axios from "axios";
 import { RestUrls } from "./URLs";
+import { async } from "q";
 
 export class RestDataSource {
     
-    GetData = (dataType) => this.SendRequest("get", RestUrls[dataType]);
+    GetData =  async(dataType, params) => 
+        this.SendRequest("get", RestUrls[dataType],params);
 
-    SendRequest = (method, url) => Axios.request({method, url});
+    SendRequest = (method, url, params) => Axios.request({method, url, params});
 }
--- src/shop/CategoryNavigtion.js
+++ src/shop/CategoryNavigtion.js
@@ -1,18 +1,19 @@
-import React, {Component} from "react";
-//import { Link } from "react-router-dom";
+import React, { Component } from "react";
 import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
 
     render() {
         return <React.Fragment>
-            <ToggleLink className="btn btn-secondary btn-block" 
-                to={ this.props.baseUrl }>All</ToggleLink>
-            { this.props.categories && this.props.categories.map(cat =>
-                <ToggleLink className="btn btn-secondary btn-block" key={ cat }
-                to={ `${this.props.baseUrl}/${cat.toLowerCase()}`}>
-                    {cat}
-                </ToggleLink>)}
+            <ToggleLink to={ `${this.props.baseUrl}/all` } exact={ false }>
+                All
+            </ToggleLink>                    
+            { this.props.categories && this.props.categories.map(cat => 
+                <ToggleLink key={ cat }
+                    to={ `${this.props.baseUrl}/${cat.toLowerCase()}`}>
+                    { cat }
+                </ToggleLink>
+            )}   
         </React.Fragment>
     }
 }
